68a9ea75d3e5fd750702ef9551989c811d13c3bf
Manish <rajhans.manish7789@gmail.com>
2018-02-06 10:03:46 +0530

    Initial Commit

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		data.display();
+	}
+
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,7 @@
+
+public class SalesData {
+	int data[] = {0,4,42};
+	public void display(){
+		System.out.println("Welcome to Sales Data Management Tool");		
+	}
+}
